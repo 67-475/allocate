2f6a698725f26d29af35424b0b59eaa8421eb50b
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-10-18 20:50:25 -0400

    outlined tests for scrambler

---
+++ test/scrambler.js
@@ -0,0 +1,7 @@
+var assert = require('chai').assert;
+const scrambler = require('../auth/scrambler')
+
+describe('scrambler', () => {
+  it('should correctly encrypt and decrypt words')
+  it('should not have the same encryption across instances')
+})
