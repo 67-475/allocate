cdc62cfeed6a4f80d3064ef34ecb5df4706b3da6
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-10-24 20:14:15 -0400

    started linter

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'extends': 'airbnb',
+    'rules': {
+        'semi': ['off'],
+        'quotes': ['off', 'single'],
+        'import/newline-after-import': ['off'],
+        'space-before-function-paren': ['off'],
+        'comma-dangle': ['off'],
+        'prefer-template': ['off']
+    }
+};
