545278e96db6f1c2ce77db4d50f8d1b1ffda0834
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-10-24 20:37:41 -0400

    linting for app/home.js

--- app/home.js
+++ app/home.js
@@ -1,8 +1,9 @@
+/* eslint  no-console:0 */
+
 var google = require('googleapis')
 var calendar = google.calendar('v3')
 
-/* takes an oauth2client, and returns an array of events*/
-
+/* takes an oauth2client, and returns an array of events */
 function home (oauth2Client, callback) {
   calendar.events.list({
     auth: oauth2Client,
@@ -12,11 +13,11 @@ function home (oauth2Client, callback) {
     singleEvents: true,
     orderBy: 'startTime',
   }, (err, response) => {
-    if(err) {
-      console.error('The API returned: '+ err)
+    if (err) {
+      console.error('The API returned: ' + err)
     } else {
       var events = response.items
-      if(events.length == 0) {
+      if (events.length === 0) {
         callback('No upcoming events')
       } else {
         callback(events)
