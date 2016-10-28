d95cb6daff5830dd84bc117269c4f6d1ef567bd5
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-10-28 15:42:11 -0400

    added regex test for bestTime

--- models/settings.js
+++ models/settings.js
@@ -6,7 +6,10 @@ var schema = {
   id: '/settings',
   type: 'object',
   properties: {
-    bestTime: { type: 'string' },
+    bestTime: {
+      type: 'string',
+      pattern: '^[man]{1}$',
+    },
     sleepTime: {
       type: 'array',
       items: {
@@ -19,19 +22,29 @@ var schema = {
 }
 buster.addSchema(schema, '/settings')
 
+/* an example settings object would take the following form
+  {
+    bestTime: "m",
+    sleepTime: [0, 800]
+  }
+ */
+
 
 /**
- * Validate given object against above schema, which
- * in practice would take the following form:
- var example = {
- bestTime: "m",
- sleepTime: [0, 800]
-}
+ * Validate given object against above schema, which requires this basic form:
+ *
+ * bestTime: either m or a or n (morning, afternoon, or night)
+ *
+ * sleepTime: an array of 2 numbers in military time
  * @param  {Object} obj a settings object
  * @return {Boolean} results Errors generated in validating the object
  */
  exports.check = (obj) => {
   const result = buster.validate(obj, schema)
-  result.errors.forEach((error) => { console.log(error) })
+  result.errors.forEach((error) => {
+    if(process.env.NODE_ENV == 'test') {
+      console.log(error)
+    }
+  })
   return result.errors
 }
--- test/models/settings.js
+++ test/models/settings.js
@@ -1,8 +1,39 @@
 const settings = require('../../models/settings')
+var assert = require('chai').assert;
+
+var morningPerson = {}
+var nightOwl = {}
 
 describe('models/settings.js', () => {
-  it('should validate a correct settings object')
-  it('should only allow the correct bestTime attributes')
+  before(() => {
+    morningPerson = {
+      bestTime: 'm',
+      sleepTime: [2200, 600]
+    }
+
+    nightOwl = {
+      bestTime: 'n',
+      sleepTime: [200, 1000]
+    }
+  })
+
+  it('should validate a correct settings object', () => {
+    assert.equal(settings.check(morningPerson).length, 0)
+    assert.equal(settings.check(nightOwl).length, 0)
+  })
+  it('should only allow the correct bestTime attributes', () => {
+    nightOwl.bestTime = 'time to get a watch'
+    assert.equal(settings.check(nightOwl).length, 1)
+
+    nightOwl.bestTime = 'man'
+    assert.equal(settings.check(nightOwl).length, 1)
+
+    nightOwl.bestTime = 'morning'
+    assert.equal(settings.check(nightOwl).length, 1)
+
+    nightOwl.bestTime = 'n'
+    assert.equal(settings.check(nightOwl).length, 0)
+  })
   it('should only allow the correct time of day')
   it('should not allow incorrect ordering of time of day')
 })
