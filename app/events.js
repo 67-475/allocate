272d9898cf8e5b84a7b1906cce0a59fab5657be8
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-11-06 16:41:09 -0500

    refactor createEvent to conform with API requisites https://github.com/google/google-api-nodejs-client/issues/565

--- app/events.js
+++ app/events.js
@@ -2,25 +2,29 @@
 var google = require('googleapis')
 var calendar = google.calendar('v3')
 
-
 /**
  * Creates a Google Calendar event with a given calendar ID
  * @param {OAuth2} oauth2Client Authorized auth instance of google.auth.OAuth2
                                 with which to make API call
  * @param {Object} event Event data to be pushed to Google
- * @param {string} event.start Start time for event
- * @param {string} event.end End time for event
+ * @param {Date} event.start Start time for event
+ * @param {Date} event.end End time for event
  * @param {string} event.title Title text for event
  * @param {function} callback function to be called after API call is made
  * @param {String} calendarId Calendar on which to put this event, defaults to primary
  */
 function createEvent(oauth2Client, event, callback, calendarId = 'primary') {
+  const start = event.start.toISOString()
+  const end = event.end.toISOString()
+
   calendar.events.insert({
     auth: oauth2Client,
     calendarId: calendarId,
-    timeMax: event.start,
-    timeMin: event.end,
-    text: event.title
+    resource: {
+      start: { dateTime: start },
+      end: { dateTime: end },
+      summary: event.summary
+    }
   }, (err, response) => {
     if (err) {
       console.error(err.stack)
