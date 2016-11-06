b8da6d3bd72c69474dba75c4710c9026d7283e1d
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-11-06 17:40:19 -0500

    moved home route to centralized event logic

--- auth/routes.js
+++ auth/routes.js
@@ -108,7 +108,7 @@ function authorize (req, res) {
 }
 
 
-var home = require('../app/home.js')
+var events = require('../app/events.js')
 /**
  * Get events from Google Calendar
  * @param  {Object} req express.js request
@@ -118,7 +118,7 @@ function getHomeEvent(req, res) {
   const email = scrambler.decrypt(req.cookies.auth)
   var client = oauth2Clients[email]
 
-  home(client, (events) => {
+  events.home(client, (events) => {
     res.render('home', {
       events: events,
       email: email
