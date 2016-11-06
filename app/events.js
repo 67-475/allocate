4056a843f1413ed733801d26f8ff2f397be3b2e5
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-11-06 15:53:23 -0500

    initial implementation for event creation, still needs testing

--- app/events.js
+++ app/events.js
@@ -1,3 +1,4 @@
+/* eslint no-console:0 */
 var google = require('googleapis')
 var calendar = google.calendar('v3')
 
@@ -7,9 +8,27 @@ var calendar = google.calendar('v3')
  * @param {OAuth2} oauth2Client Authorized auth instance of google.auth.OAuth2
                                 with which to make API call
  * @param {Object} event Event data to be pushed to Google
+ * @param {string} event.start Start time for event
+ * @param {string} event.end End time for event
+ * @param {string} event.title Title text for event
  * @param {function} callback function to be called after API call is made
- * @param {String} calendarID Calendar on which to put this event, defaults to primary
+ * @param {String} calendarId Calendar on which to put this event, defaults to primary
  */
-function createEvent(oauth2Client, event, callback, calendarID = 'primary') {
-  
+function createEvent(oauth2Client, event, callback, calendarId = 'primary') {
+  calendar.events.insert({
+    auth: oauth2Client,
+    calendarId: calendarId,
+    timeMax: event.start,
+    timeMin: event.end,
+    text: event.title
+  }, (err, response) => {
+    if (err) {
+      console.error(err.stack)
+    } else {
+      console.log(response)
+      callback(response)
+    }
+  })
 }
+
+module.exports = createEvent
