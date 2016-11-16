2f00701520461b3475fcdce076bc970f3973491d
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-11-16 18:37:25 -0500

    added skeleton for how event verification would work

--- app/allocate.js
+++ app/allocate.js
@@ -4,6 +4,30 @@ var events = require('./events')
 const ONE_DAY = 1000 * 60 * 60 * 24
 
 /**
+ * Format events based on given input
+ * @param  {Date} start
+ * @param  {Integer} length
+ * @param  {Integer} daysAfter
+ * @return {Object} event
+ */
+function createEvent(start, length, daysAfter) {
+  return {
+    start: start.getTime() + ONE_DAY * daysAfter,
+    end: start.getTime() + ONE_DAY * daysAfter + length
+  }
+}
+
+/**
+ * Whether to events Overlap
+ * @param  {Object} allocatedEvent
+ * @param  {Object} calendarEvent
+ * @return {boolean} result
+ */
+function doesOverlap(allocatedEvent, calendarEvent) {
+  return false
+}
+
+/**
  * Actually divide up project into events based on a given email's calendar
  * Will call events.createEvent with each generated event
  * @param {Object} project A description of the overall event
@@ -21,20 +45,17 @@ function divvy(project, oauth2Client, callback) {
     const days = Math.floor((project.end - project.start) / ONE_DAY) + 1
     var length = Math.floor(project.hours / days)
     // now will allocate a slot each day over the course of days days
-    for(var i = 0; i < days; i ++) {
+    for (var i = 0; i < days; i++) {
       allocatedEvents.push(createEvent(project.start, length, i))
     }
     console.log(allocatedEvents)
+    while (true) {
+      // implement checking overlaps
+      break;
+    }
 
     callback(true)
   })
-}
-
-function createEvent(start, length, daysAfter) {
-  return {
-    start: start.getTime() + ONE_DAY * daysAfter,
-    end: start.getTime() + ONE_DAY * daysAfter + length
-  }
 }
 
 /**
--- app/events.js
+++ app/events.js
@@ -35,6 +35,22 @@ function createEvent(oauth2Client, event, callback, calendarId = 'primary') {
 }
 
 /**
+ * Format Date to conform with what Google APIs want
+ * @param {Date} date
+ * @return {String} representation
+ */
+function formatDate (date) {
+  var iso = date.toISOString()
+  iso = iso.substring(0, iso.length - 1)
+  var offset = (date.getTimezoneOffset() / 100) + 1 + ":00"
+  if (offset.length < 5) {
+    offset = "0" + offset
+  }
+
+  return iso + "-" + offset
+}
+
+/**
   * Pulls Google Calendar events given a client
   * @param {Object} project Project in which the span of events are
   * @param {OAuth2} oauth2Client Authorized auth instance of google.auth.OAuth2
@@ -55,19 +71,6 @@ function getEvents (project, oauth2Client, callback) {
     }
   })
 }
-
-function formatDate (date) {
-  var iso = date.toISOString()
-  iso = iso.substring(0, iso.length - 1)
-  var offset = (date.getTimezoneOffset() / 100) + 1
-  offset = offset + ":00"
-  if (offset.length < 5) {
-    offset = "0" + offset
-  }
-
-  return iso + "-" + offset
-}
-
 
 module.exports = {
   createEvent: createEvent,
